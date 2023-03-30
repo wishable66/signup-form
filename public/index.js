3b1066d433a802dc64db101968fbc407e06537df
Wishable <gjbbub05@gmail.com>
2023-03-29 20:46:32 -0600

    Add validation for password === confirmPassword

--- public/index.js
+++ public/index.js
@@ -2,15 +2,36 @@ const form = document.querySelector('form');
 function updateError(errorElement, errorMessage) {
     return (errorElement.textContent = errorMessage);
 }
-form.addEventListener('submit', (e) => e.preventDefault());
+function validatePassword() {
+    const password = (document.querySelector('#password')).value;
+    const confirmPassword = (document.querySelector('#confirmpass')).value;
+    if (!confirmPassword || !password)
+        return '1pass';
+    if (password === confirmPassword) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
 form.addEventListener('change', (e) => {
+    const confirmPassEl = (document.querySelector('#confirmpass').nextElementSibling);
     const target = e.target;
     const inputType = target.id.replace('#', '');
     const inputValue = target.value;
     const errorDiv = target.nextElementSibling;
     const validity = target.validity;
+    console.log(validatePassword());
     if (validity.valid)
-        return updateError(errorDiv, '');
+        updateError(errorDiv, '');
+    if (inputType === 'password' || inputType === 'confirmpass') {
+        if (!validatePassword()) {
+            updateError(confirmPassEl, '* Password does not match');
+        }
+    }
     if (validity.tooShort) {
         updateError(errorDiv, `* ${inputType} is too short! (${inputValue.length.toString()}/${target.getAttribute('minlength')})`);
     }
--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,45 @@ function updateError(errorElement: HTMLDivElement, errorMessage: string) {
   return (errorElement.textContent = errorMessage);
 }
 
-form.addEventListener('submit', (e) => e.preventDefault());
+function validatePassword() {
+  const password: string = (<HTMLInputElement>(
+    document.querySelector('#password')
+  )).value;
+  const confirmPassword: string = (<HTMLInputElement>(
+    document.querySelector('#confirmpass')
+  )).value;
+
+  if (!confirmPassword || !password) return '1pass';
+  if (password === confirmPassword) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
 
 form.addEventListener('change', (e: Event) => {
+  const confirmPassEl = <HTMLInputElement>(
+    document.querySelector('#confirmpass').nextElementSibling
+  );
   const target = <HTMLFormElement>e.target;
   const inputType = target.id.replace('#', '');
   const inputValue = target.value;
   const errorDiv = <HTMLDivElement>target.nextElementSibling;
   const validity = <ValidityState>target.validity;
 
-  if (validity.valid) return updateError(errorDiv, '');
+  console.log(validatePassword());
+
+  if (validity.valid) updateError(errorDiv, '');
+
+  if (inputType === 'password' || inputType === 'confirmpass') {
+    if (!validatePassword()) {
+      updateError(confirmPassEl, '* Password does not match');
+    }
+  }
 
   if (validity.tooShort) {
     updateError(
